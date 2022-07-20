56c6e3cfe54f592a69f03425ad4fe4a27723b101
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-20 21:17:50 +0100

    turn_player
    Player turn number.

--- mdn-webdocs/guess-the-number/script.js
+++ mdn-webdocs/guess-the-number/script.js
@@ -1,8 +1,12 @@
-let randomNumber = Math.floor(Math.random() * 11);
+let randomNumber = Math.floor(Math.random() * 11); // random number
+let guessAttempt = 1; // which attempt
 console.log(randomNumber);
 
 let guessSubmit = document.getElementById("guessSubmit");
 
+// on click button function
 guessSubmit.addEventListener("click", () => {
   console.log(Math.floor(Math.random() * 11));
+  guessAttempt++; // after submiting incresse
+  console.log(guessAttempt);
 });
