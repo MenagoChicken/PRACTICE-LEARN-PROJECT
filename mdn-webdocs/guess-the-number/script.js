c4a4008686b81eaadde256775f7ed9c5646813da
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-20 21:13:14 +0100

    random_number
    random number generator and button event Listener

--- mdn-webdocs/guess-the-number/script.js
+++ mdn-webdocs/guess-the-number/script.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+let randomNumber = Math.floor(Math.random() * 11);
+console.log(randomNumber);
+
+let guessSubmit = document.getElementById("guessSubmit");
+
+guessSubmit.addEventListener("click", () => {
+  console.log(Math.floor(Math.random() * 11));
+});
