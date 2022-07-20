b1c5b3215e090296d8ac1453eb38e38b0a48ff4c
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-20 21:34:13 +0100

    store_guesses
    Store user guesses.

--- mdn-webdocs/guess-the-number/script.js
+++ mdn-webdocs/guess-the-number/script.js
@@ -1,13 +1,19 @@
 let randomNumber = Math.floor(Math.random() * 11); // random number
 let guessAttempt = 1; // which attempt
+let userGuessArrray = [];
 
 console.log(randomNumber);
 let guessField = document.getElementById("guessField");
 let guessSubmit = document.getElementById("guessSubmit");
 
+function addUserGuess(userGuess) {
+  userGuessArrray.push(userGuess);
+}
+
 // on click button function
 guessSubmit.addEventListener("click", () => {
   guessAttempt++; // after submiting incresse
   //The value property sets or returns the value of the value attribute of a text field.
-  console.log(guessField.value);
+  addUserGuess(guessField.value);
+  console.log(userGuessArrray);
 });
