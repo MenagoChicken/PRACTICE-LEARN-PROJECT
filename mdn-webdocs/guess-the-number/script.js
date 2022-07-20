5f4a368dbed61545b008e90df3007bb0dda0f547
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-20 22:35:07 +0100

    congratulation_message
    Congratulation message.

--- mdn-webdocs/guess-the-number/script.js
+++ mdn-webdocs/guess-the-number/script.js
@@ -2,15 +2,19 @@ let randomNumber = Math.floor(Math.random() * 11); // random number
 let guessAttempt = 1; // which attempt
 let userGuessArrray = []; // user guesses
 
+console.log(randomNumber);
+
 // html elements
 let guessField = document.getElementById("guessField");
 let guessSubmit = document.getElementById("guessSubmit");
 let userGuesses = document.querySelector(".userGuesses");
+let congratulations = document.querySelector(".congratulations");
 
 // on click button function
 guessSubmit.addEventListener("click", () => {
   guessAttempt++; // after submiting incresse
   if (Number.isInteger(parseInt(guessField.value))) {
+    isTheRandomNumber(guessField.value);
     //The value property sets or returns the value of the value attribute of a text field.
     let niceFormat = " " + guessField.value;
     //add guess to the table
@@ -28,4 +32,14 @@ guessSubmit.addEventListener("click", () => {
 
 function addUserGuess(userGuess) {
   userGuessArrray.push(userGuess);
+}
+
+function isTheRandomNumber(number) {
+  if (number == randomNumber) {
+    congratulations.innerText =
+      "Congratulations you've guessed the number! Indeed it was \u2192 " +
+      randomNumber;
+  }
+
+  return;
 }
