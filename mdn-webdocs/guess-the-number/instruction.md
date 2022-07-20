815adfa3aa31cd06ba1cb2b69812e082c774a1ca
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-20 21:28:15 +0100

    get_input
    Get input from user.

--- mdn-webdocs/guess-the-number/instruction.md
+++ mdn-webdocs/guess-the-number/instruction.md
@@ -1,6 +1,6 @@
 1. Generate a random number between 1 and 100. - done
 2. Record the turn number the player is on. Start it on 1. - done
-3. Provide the player with a way to guess what the number is.
+3. Provide the player with a way to guess what the number is. - done
 4. Once a guess has been submitted first record it somewhere so the user can see their previous guesses.
 5. Next, check whether it is the correct number.
 6. If it is correct:
--- mdn-webdocs/guess-the-number/script.js
+++ mdn-webdocs/guess-the-number/script.js
@@ -1,12 +1,13 @@
 let randomNumber = Math.floor(Math.random() * 11); // random number
 let guessAttempt = 1; // which attempt
-console.log(randomNumber);
 
+console.log(randomNumber);
+let guessField = document.getElementById("guessField");
 let guessSubmit = document.getElementById("guessSubmit");
 
 // on click button function
 guessSubmit.addEventListener("click", () => {
-  console.log(Math.floor(Math.random() * 11));
   guessAttempt++; // after submiting incresse
-  console.log(guessAttempt);
+  //The value property sets or returns the value of the value attribute of a text field.
+  console.log(guessField.value);
 });
