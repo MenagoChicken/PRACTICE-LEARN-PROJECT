f5564f5853e30e1b061d350fbfe19762efe0b409
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-30 17:13:28 +0100

    _working button
    Randomize button shows paragraph.

--- mdn-webdocs/silly-story-generator/main.js
+++ mdn-webdocs/silly-story-generator/main.js
@@ -1,1 +1,17 @@
-console.log("Hello  World");
+/*********** VARIABLES ***********/
+const customName = document.querySelector("#customname");
+const story = document.querySelector(".story");
+const randomizeButton = document.querySelector(".randomize");
+const storyString =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. \
+  When they got to :inserty:, they stared in horror for a few moments, \
+  then :insertz:. Bob saw the whole thing, but was not surprised \
+   — :insertx: weighs 300 pounds, and it was a hot day.";
+
+/*********** VARIABLES END ***********/
+
+randomizeButton.addEventListener("click", () => {
+  console.log("Randomize click!");
+  story.textContent = storyString;
+  story.style.visibility = "visible";
+});
