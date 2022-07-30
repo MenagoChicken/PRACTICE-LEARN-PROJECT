c770e0c6faaf151f73fdbe3b19f4a90108a25a21
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-30 21:18:34 +0100

    simple version finished
    I think this is it. Everything works.

--- how-many-words/app.js
+++ how-many-words/app.js
@@ -7,18 +7,18 @@ console.log(counter);
 textStorage.addEventListener("input", () => {
   const pasedStringLength = textStorage.value.length;
 
-  let wordsArray = textStorage.value.split(" ");
+  const wordsArray = textStorage.value.split(/\s/g);
   let letterCounter = 0;
   let wordCounter = 0;
   for (const word of wordsArray) {
-    if (word !== "") {
-      let simpleWord = word.replace(/[^a-zA-Ząćęłóśżź]/g, "");
+    if (word.length !== 0) {
+      const simpleWord = word.replace(/[^a-zA-Ząćęłóśżź]/g, "");
       letterCounter += simpleWord.length;
       wordCounter++;
     }
   }
 
-  console.log();
+  console.log(wordsArray);
   const newCounterValue = `Words: ${wordCounter} || Letters: ${letterCounter} || Characters: ${pasedStringLength}`;
   displayedCounter.textContent = newCounterValue;
 });
