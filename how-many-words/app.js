4eeaaa45d3588e9ae5e0e4856a1a3f034d3058a4
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-30 19:00:29 +0100

    working_Listener
    Variables and working listener

--- how-many-words/app.js
+++ how-many-words/app.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+/*********** VARIEABLES */
+const textStorage = document.getElementById("text-storage");
+const counter = document.querySelector("#counter");
+console.log(textStorage);
+console.log(counter);
+
+textStorage.addEventListener("input", () => {
+  console.log("Pressing buttons");
+  console.log(textStorage.value);
+});
