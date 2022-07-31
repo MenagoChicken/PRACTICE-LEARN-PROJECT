bb6fc573f3db0a1977dcc090a1c71ad8738a40e6
Lewandowski <39875881+MenagoChicken@users.noreply.github.com>
2022-07-31 08:43:10 +0100

    testing_maps
    Trying the best way to do words stats.

---
+++ how-many-words/test.js
@@ -0,0 +1,34 @@
+const words = [
+  "look",
+  "word",
+  "book",
+  "love",
+  "work",
+  "love",
+  "book",
+  "word",
+  "JavaScript",
+  "something",
+  "love",
+  "hate",
+  "done",
+  "JavaScript",
+  "JavaScript",
+  "JavaScript",
+  "something",
+  "love",
+  "hate",
+  "done",
+  "book",
+  "word",
+  "JavaScript",
+  "something",
+  "love",
+  "hate",
+];
+
+const wordsMap = new Map();
+
+for (const word of words) {
+  console.log(word);
+}
